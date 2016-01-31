3137d3eceb936309850b8cf7725e7b2686d4850b
Nico <nico97953@gmail.com>
2016-01-31 20:36:28 -0300

    Creaccion carpeta de prueba

---
+++ pruebas-php/header.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<title> Muestra de includes </title>
+
+</head>
+<body> 
---
+++ pruebas-php/index.php
@@ -0,0 +1,5 @@
+<?php include("header.php"); ?>
+<p>
+Hola, este es el contenido.
+</p>
+<?php include("footer.php"); ?> 
