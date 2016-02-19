53331d82438828443066dc38474b3613f65bb149
carlos <andjustice@gmail.com>
2016-02-19 20:50:46 -0300

    caca fresca

--- pruebas-php/header.php
+++ pruebas-php/header.php
@@ -2,7 +2,7 @@
 <head>
   <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
-  <link rel="stylesheet" href="estilos-pruebas.css" media="screen" title="no title" charset="utf-8">
+  <link rel="stylesheet" href="css/estilos-pruebas.css" media="screen" title="no title" charset="utf-8">
   <link href='https://fonts.googleapis.com/css?family=Droid+Serif:700' rel='stylesheet' type='text/css'>
 </head>
 <body>
