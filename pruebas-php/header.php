caa8b18a243f7e8bf2339e05400fdb10ab7eba02
carlos@sta.marketing <carlos@carloss-MacBook-Pro.local>
2016-06-14 23:21:36 -0300

    borrado general

--- pruebas-php/header.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
-  <link rel="stylesheet" href="css/estilos-pruebas.css" media="screen" title="no title" charset="utf-8">
-  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:700' rel='stylesheet' type='text/css'>
-</head>
-<body>
